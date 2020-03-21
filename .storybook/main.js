3b813093251d113c11dbcacc915c4c02964dfdee
Ulrich Helt Green <ugreen@gmail.com>
2020-03-21 22:21:19 +0100

    feat(infra): Set up storybook to use typescript and mdx (addon-docs)

---
+++ .storybook/main.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = {
+    stories: ["../src/stories/**/*.stories.(ts|tsx|js|jsx|mdx)"],
+    addons: [
+        "@storybook/addon-actions",
+        "@storybook/addon-links",
+        "@storybook/preset-create-react-app",
+        {
+            name: "@storybook/addon-docs",
+            options: {
+                configureJSX: true
+            }
+        }
+    ]
+};
