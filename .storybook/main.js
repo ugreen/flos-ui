372f69b397ba1c4321e83c38c4ad8c6e99921df0
Ulrich Helt Green <ugreen@gmail.com>
2020-03-24 20:10:58 +0100

    feat(infra): Add example components and configure addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,18 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-    stories: ["../src/stories/**/*.stories.(ts|tsx|js|jsx|mdx)"],
+    stories: [
+        '../src/stories/welcome.stories.tsx',
+        '../src/**/*.stories.(ts|tsx|js|jsx|mdx)'
+    ],
     addons: [
-        "@storybook/addon-actions",
-        "@storybook/addon-links",
-        "@storybook/preset-create-react-app",
+        '@storybook/addon-actions',
+        '@storybook/addon-links',
+        '@storybook/preset-create-react-app',
+        '@storybook/addon-storysource/register',
+        '@storybook/addon-knobs/register',
         {
-            name: "@storybook/addon-docs",
+            name: '@storybook/addon-docs',
             options: {
                 configureJSX: true
             }
---
+++ src/shared/story-link-wrapper.tsx
@@ -0,0 +1,37 @@
+/* eslint-disable import/no-extraneous-dependencies */
+// This is allows us to test whether the link works via the actions addon
+import React, { ReactNode} from 'react';
+import { action } from '@storybook/addon-actions';
+
+const fireClickAction = action('onLinkClick');
+
+export interface StoryLinkWrapperProps {
+  children: ReactNode;
+  className?: string;
+  href?: string;
+  onClick?: Function;
+  to?: string;
+}
+
+function StoryLinkWrapper ({ children, className, href, onClick, to, ...rest }: StoryLinkWrapperProps) {
+  const modifiedOnClick = (event: any) => {
+    event.preventDefault();
+    onClick && onClick();
+    fireClickAction(href || to);
+  };
+
+  return (
+    <a className={className} href={href || to} onClick={modifiedOnClick} {...rest}>
+      {children}
+    </a>
+  );
+}
+
+StoryLinkWrapper.defaultProps = {
+  className: null,
+  href: null,
+  onClick: () => {},
+  to: null,
+};
+
+export default StoryLinkWrapper;
